e4a9595d830a08879893eacfa853b41dafa990b7
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-04-18 13:20:05 +0900

    datamapper bug?

--- db/migrate/003_add_enabled_bitly_to_project.rb
+++ db/migrate/003_add_enabled_bitly_to_project.rb
@@ -1,7 +1,7 @@
 migration 3, :add_enabled_bitly_to_project do
   up do
     modify_table :projects do
-      add_column :enabled_bitly, Boolean
+      add_column :enabled_bitly, "BOOLEAN"
     end
   end
 
