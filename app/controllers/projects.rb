11f352ca4319fe40a5c7271df1703f1621b15050
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-04-20 19:30:30 +0900

    opps

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -16,7 +16,7 @@ Pivotter.controllers :projects do
 		}
 
 		ShoutBot.shout(@project.irc_channel) do |channel|
-			channel.whisper "^C3#{author}^C#{desc} - #{links.join(' ')}"
+			channel.whisper "3#{author}#{desc} - #{links.join(' ')}"
 		end
 	end
 end
