09d98f1fb6ad5e58f13d95a3d93b5903c1bcb4cc
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-04-23 19:22:11 +0900

    drop unneccesarry columns.

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,8 +5,6 @@ class Account
 
   # Properties
   property :id,               Serial
-  property :name,             String
-  property :surname,          String
   property :email,            String
   property :crypted_password, String
   property :salt,             String
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ password  = shell.ask "Tell me the password to use:"
 
 shell.say ""
 
-account = Account.create(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
+account = Account.create(:email => email, :password => password, :password_confirmation => password, :role => "admin")
 
 if account.valid?
   shell.say "================================================================="
