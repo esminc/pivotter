fad780bdb9784620755939c198bb5adaf51a330c
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-06-20 16:08:36 +0900

    upgrade padrino-0.9.13

--- config/apps.rb
+++ config/apps.rb
@@ -17,5 +17,5 @@ However, the mounted core can be modified as needed:
 =end
 
 # Mounts the core application for this project
-Padrino.mount_core("Pivotter")
+Padrino.mount("Pivotter").to("/")
 Padrino.mount("Admin").to("/admin")
