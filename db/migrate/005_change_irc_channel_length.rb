9e14ac60f74cfb403ae924030bbc6efced6239e1
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-07-26 19:03:20 +0900

    s/string/varchar

--- db/migrate/005_change_irc_channel_length.rb
+++ db/migrate/005_change_irc_channel_length.rb
@@ -1,14 +1,13 @@
 migration 5, :change_irc_channel_length do
   up do
     modify_table :projects do
-      change_column :irc_channel, "STRING", :length => 100
+      change_column :irc_channel, "varchar(100)"
     end
   end
 
   down do
     modify_table :projects do
-      change_column :irc_channel, "STRING"
+      change_column :irc_channel, "varchar(50)"
     end
   end
 end
-
