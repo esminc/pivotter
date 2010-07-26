0017136e95d256ba694d5fd8dc19e13e735f8991
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-07-26 18:44:00 +0900

    s/size/length

--- db/migrate/005_change_irc_channel_length.rb
+++ db/migrate/005_change_irc_channel_length.rb
@@ -1,7 +1,7 @@
 migration 5, :change_irc_channel_length do
   up do
     modify_table :projects do
-      change_column :irc_channel, "STRING", :size => 100
+      change_column :irc_channel, "STRING", :length => 100
     end
   end
 
@@ -11,3 +11,4 @@ migration 5, :change_irc_channel_length do
     end
   end
 end
+
