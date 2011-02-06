c53876151e504c55cd98d75d006dad816878f31e
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-02-06 17:42:57 +0900

    oops...

--- db/migrate/006_change_email_to_name_for_account.rb
+++ db/migrate/006_change_email_to_name_for_account.rb
@@ -1,13 +1,13 @@
-migration 6, :change_name_to_name_for_account do
+migration 6, :change_email_to_name_for_account do
   up do
     modify_table :accounts do
-      rename_column :name, :name
+      rename_column :email, :name
     end
   end
 
   down do
     modify_table :accounts do
-      change_column :name, :name
+      rename_column :name, :email
     end
   end
 end
