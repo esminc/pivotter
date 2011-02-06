33ecda2e12c540e83b063fd79df996d8b9f171be
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-02-06 13:34:33 +0900

    delete unused spec

--- spec/models/project_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
-
-describe "Project Model" do
-  it 'can be created' do
-    @project = Project.new
-    @project.should_not be_nil
-  end
-end
